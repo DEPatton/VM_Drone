eca0fdc7fa3caee018d918270bb5f3dd733b85f3
patto <depatton@student.fullsail.edu>
2023-12-13 12:09:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vm_drone/StartUp.java
@@ -0,0 +1,18 @@
+package com.example.vm_drone;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StartUp extends Activity
+{
+    //TextView edit;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        //edit.findViewById(R.id.HelloWorld);
+    }
+
+}
